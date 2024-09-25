9e049fc8d597d570a8e981046def578029a2b212
Rob-Tor16 <roberto1608torales@gmail.com>
2024-09-25 14:42:41 -0300

    se termino el html, css y se declararon elementos en main js

---
+++ main.js
@@ -0,0 +1,15 @@
+const monedaUno = docuemnt.getElementById("moneda-uno");
+const monedaDos = docuemnt.getElementById("moneda-dos");
+const cantidadUno = docuemnt.getElementById("cantidad-uno");
+const cantidadDos = docuemnt.getElementById("cantidad-dos");
+const cambio = docuemnt.getElementById("cambio");
+const taza = docuemnt.getElementById("taza");
+
+
+function calculate(){
+
+
+
+}
+
+calculate();
